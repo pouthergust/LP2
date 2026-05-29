88d91f624f0193bd10b0b00b8d560590c43b3f30
GABRIEL HENRIQUE VIEIRA NUNES <0030482523046@lti.fatecso.edu.br>
2026-05-29 20:24:55 -0300

    Exam folder added

--- atividade3/atividade3/obj/Debug/net8.0-windows/atividade3.AssemblyInfo.cs
+++ atividade3/atividade3/obj/Debug/net8.0-windows/atividade3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atividade3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b557d5227bb7a8db2e81e5e34d3f6c93785a341")]
 [assembly: System.Reflection.AssemblyProductAttribute("atividade3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atividade3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
